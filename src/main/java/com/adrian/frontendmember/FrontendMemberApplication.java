7fec4a153114d8ad5ea77e559859357381f57286
Adriancoding96 <adrian.nilsson.coding@gmail.com>
2023-11-05 17:08:02 +0100

    Frontend för mitt member-api

---
+++ src/main/java/com/adrian/frontendmember/FrontendMemberApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.frontendmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrian/frontendmember/FrontendMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.frontendmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
